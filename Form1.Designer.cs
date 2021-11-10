0763dfb527a069bd0a0ba56c98591c3a00d17852
opilane <opilane@HP-CZC6297B8P>
2021-11-10 15:42:57 +0200

    0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,42 +29,132 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
+            this.run_button = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.valdkonnas = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.väärtus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // run_button
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.button1.FlatAppearance.BorderSize = 10;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Mongolian Baiti", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(616, 96);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(170, 78);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.run_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.run_button.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.run_button.FlatAppearance.BorderSize = 10;
+            this.run_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.run_button.Font = new System.Drawing.Font("Mongolian Baiti", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.run_button.Location = new System.Drawing.Point(430, 114);
+            this.run_button.Name = "run_button";
+            this.run_button.Size = new System.Drawing.Size(170, 78);
+            this.run_button.TabIndex = 0;
+            this.run_button.Text = "Start";
+            this.run_button.UseVisualStyleBackColor = false;
+            this.run_button.Click += new System.EventHandler(this.run_button_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(529, 194);
+            this.pictureBox1.Location = new System.Drawing.Point(380, 221);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(293, 166);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.valdkonnas,
+            this.väärtus});
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(288, 296);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // valdkonnas
+            // 
+            this.valdkonnas.Text = "valdkonnas";
+            this.valdkonnas.Width = 72;
+            // 
+            // väärtus
+            // 
+            this.väärtus.Text = "väärtus";
+            this.väärtus.Width = 104;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(206, 315);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(95, 20);
+            this.txtA.TabIndex = 3;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(206, 341);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(95, 20);
+            this.txtB.TabIndex = 4;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(206, 367);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(95, 20);
+            this.txtC.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noto Sans Hebrew", 9.749999F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(148, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "pool A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Noto Sans Hebrew", 9.749999F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(148, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "pool B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Noto Sans Hebrew", 9.749999F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(148, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "pool C";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 479);
+            this.ClientSize = new System.Drawing.Size(730, 414);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.run_button);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -72,13 +162,23 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button run_button;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader valdkonnas;
+        private System.Windows.Forms.ColumnHeader väärtus;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,12 +17,35 @@ namespace trriandle
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void run_button_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            a = Convert.ToDouble(txtA.Text);
+            b = Convert.ToDouble(txtB.Text);
+            c = Convert.ToDouble(txtC.Text);
+            triangle triangle = new triangle(a, b, c);
+            listView1.Items.Add("pool a:");
+            listView1.Items.Add("pool b:");
+            listView1.Items.Add("pool c:");
+            listView1.Items.Add("ümbermõõt:");
+            listView1.Items.Add("Ruut:");
+            listView1.Items.Add("on olemas?:");
+            listView1.Items.Add("täpsustaja");
+            listView1.Items[0].SubItems.Add(triangle.outPutA());
+            listView1.Items[1].SubItems.Add(triangle.outPutB());
+            listView1.Items[2].SubItems.Add(triangle.outPutC());
+            listView1.Items[3].SubItems.Add(Convert.ToString(triangle.Peremeter()));
+            listView1.Items[4].SubItems.Add(Convert.ToString(triangle.Surface()));
+            if (triangle.ExistTriangle) { listView1.Items[5].SubItems.Add("olemas"); }
+            else { listView1.Items[5].SubItems.Add("ei ole"); }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
